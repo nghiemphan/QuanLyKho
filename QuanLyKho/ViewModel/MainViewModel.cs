4c3a08c19b369aa21a781452426229c7e601d69c
nghiemphan <phantiensinh159@gmail.com>
2020-09-23 13:18:54 +0700

    first commit

---
+++ QuanLyKho/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyKho.ViewModel
+{
+    public class MainViewModel:BaseViewModel
+    {
+        public bool isloaded = false;
+        public MainViewModel()
+        {
+            if (!isloaded)
+            {
+                isloaded = true;
+                LoginWindow login = new LoginWindow();
+                login.ShowDialog();
+            }
+        }
+    }
+}
